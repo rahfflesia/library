c7c5b338253c1fa55dfeebda9779c620a6949686
rahfflesia <154933782+rahfflesia@users.noreply.github.com>
2024-06-23 19:51:54 -0700

    added functionality to the buttons and created the form for adding books

--- book.js
+++ book.js
@@ -1,5 +1,25 @@
 const myLibrary = [];
 
-let addBook = document.querySelector('.');
+const addBook = document.querySelector('.black');
+const dialog = document.querySelector('dialog');
+const closeButton = document.querySelector('.close');
+
+addBook.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
+
+function Book(title, author, numberOfPages, state){
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.state = state;
+}
+
+function addBookToLibrary(){
+}
 
 
